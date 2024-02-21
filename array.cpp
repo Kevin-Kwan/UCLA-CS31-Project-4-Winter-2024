2be998c9b21a348e1d65a12ae5bf0b25e9ef924a
Kevin Kwan <koolkev246@gmail.com>
2024-02-21 08:13:59 +0000

    starter file

---
+++ array.cpp
@@ -0,0 +1,27 @@
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cassert>
+
+int locateMaximum(const string array[], int n) {
+    return 0;
+}
+
+int countFloatingPointValues(const string array[], int n) {
+    return 0;
+}
+
+bool hasNoCapitals(const string array[], int n) {
+    return 0;
+}
+
+int shiftLeft(string array[], int n, int amount, string placeholder) {
+    return 0;
+}
+
+int main() {
+    string folks[8] = {"samwell", "jon", "margaery", "daenerys", "tyrion", "sansa", "magdalena", "jon"};
+    bool b = hasNoCapitals(folks, 5);
+    cout << "hi";
+    return 0;
+}
